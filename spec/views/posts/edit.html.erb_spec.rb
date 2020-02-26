02b9f038407f1484d679fd341124aa59e75a40a7
Riley Slayden <slaydenriley@gmail.com>
2020-02-26 19:22:10 +0000

    Automatically backed up by Learn

--- spec/views/posts/edit.html.erb_spec.rb
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -7,12 +7,5 @@ RSpec.describe "posts/edit", :type => :view do
     ))
   end
 
-  it "renders the edit post form" do
-    render
 
-    assert_select "form[action=?][method=?]", post_path(@post), "post" do
-
-      assert_select "input#post_title[name=?]", "post[title]"
-    end
-  end
 end
